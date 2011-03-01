61bd9e3545fb2ecb1c63ab29f2a87fc42ad52726
Wojciech Kupiec <kupiec.w@gmail.com>
2011-03-01 21:45:47 +0100

    Added all the other avaliable attributes. Added attribute selector

--- gvfs.c
+++ gvfs.c
@@ -67,31 +67,61 @@ PHP_FUNCTION(gvfs_info)
 {
     GFileQueryInfoFlags flags = 0x0;
     GFileInfo *info;
-    GError *error;
+    GError *error = NULL;
     GFile *file;
     char **attributes;
+    char *required_attr;
+    int required_attr_len;
     char *s;
     int i; 
     char *filename;
     int filename_len;
 
-    if(zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &filename, &filename_len) == FAILURE) {
+    if(zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s|s", &filename, &filename_len, &required_attr, &required_attr_len) == FAILURE) {
        return;
     }
-    
-    error = NULL;
+    if(required_attr_len <= 0) {
+        required_attr = "*";
+    }
+
     g_type_init ();
 
     flags |= G_FILE_QUERY_INFO_NOFOLLOW_SYMLINKS;
     file = g_file_parse_name (filename);
 
-    info = g_file_query_info (file, "standard::content-type", flags, NULL, &error);
+    info = g_file_query_info (file, required_attr, flags, NULL, &error);
 
-    if(error!=NULL) {
-        RETURN_STRING(error->message, 1);
+    if (error!=NULL) {
+        php_error_docref(NULL TSRMLS_CC, E_WARNING, error->message);
+        RETURN_FALSE;
     } else {
+        array_init(return_value);
         attributes = g_file_info_list_attributes (info, NULL);
-        s = g_file_info_get_attribute_as_string (info, *attributes);
-        RETURN_STRING(s, 1);
+
+        for (i = 0; attributes[i] != NULL; i++) {
+            /* standard::icon is non human redable list, needs to be converted and returned as a sub array */
+            if (strcmp (attributes[i], "standard::icon") == 0) {
+                GIcon *icon;
+                int j;
+                const char * const *names = NULL;
+                char *ico_item;
+                zval *icons;
+                icon = g_file_info_get_icon (info);
+                if (G_IS_THEMED_ICON(icon)) {
+                    names = g_themed_icon_get_names (G_THEMED_ICON (icon));
+                    ALLOC_INIT_ZVAL(icons);
+                    array_init(icons);
+                    for (j = 0; names[j] != NULL; j++) {
+                        add_index_string(icons, j, names[j], 1);
+                    }
+                    add_assoc_zval(return_value, attributes[i], icons);
+                    g_free (names);
+                }
+            } else {
+                s = g_file_info_get_attribute_as_string (info, attributes[i]);
+                add_assoc_string(return_value, attributes[i], s, 1);
+                g_free (s);
+            }
+        }
     }
 }
